5f40143939c5ce70b4f3bc38244038af82763d4e
isa <ion.sabatico@gmail.com>
2022-12-28 21:25:23 -0600

    added logout and token status checks

--- appFactory.py
+++ appFactory.py
@@ -1,19 +1,18 @@
-
-from flask import Flask
-from flask_smorest import Api
-from flask_jwt_extended import JWTManager
-
 import os as os
 
-# below is like models.__init__ ( we have the 2 import lines in __init__.py inside models, these will be imported automatically here)
+from flask import Flask, jsonify
+from flask_jwt_extended import JWTManager
+from flask_smorest import Api
 
 from db import db
-
-
+from models import BlocklistModel
 from resources.item import blp as ItemBlueprint
 from resources.store import blp as StoreBlueprint
 from resources.tag import blp as TagBlueprint
 from resources.user import blp as UserBlueprint
+
+# below is like models.__init__ ( we have the 2 import lines in __init__.py inside models, these will be imported automatically here)
+
 
 def create_app(db_url=None):
     app = Flask(__name__)
@@ -28,31 +27,71 @@ def create_app(db_url=None):
     app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
     app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
 
-    app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv("DATABASE_URL","sqlite:///data.db")
+    app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv("DATABASE_URL", "sqlite:///data.db")
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-    
-    #initialises sqlalchemy and connects to our app
+
+    # initialises sqlalchemy and connects to our app
     db.init_app(app)
-    
-    #this will always run after the app has started, but right before first request ( triggered automatically)
+
+    # this will always run after the app has started, but right before first request ( triggered automatically)
     @app.before_first_request
     def create_tables():
         db.create_all()
-        
-        
 
-    
-    
-    #initiate JWT manager
-    app.config["JWT_SECRET_KEY"] = os.environ.get('GWT_SECRET_KEY')
-    jwt= JWTManager(app)
-    
+    # initiate JWT manager
+    app.config["JWT_SECRET_KEY"] = os.environ.get("GWT_SECRET_KEY")
+    jwt = JWTManager(app)
+
+    @jwt.token_in_blocklist_loader
+    # builtindecorator that makes the below function run at tokent use, to check if the token is still good, or already revoked
+    def check_if_token_in_bloklist(jwt_header, jwt_payload):
+        """the function must take the 2 args from above
+        the function will check if the token "jti" value is in BLOCKLIST, and return TRUE if revoked, or false if not"""
+        jti = jwt_payload["jti"]
+
+        if BlocklistModel.query.filter(BlocklistModel.jti == jti).first():
+            return True
+        return False
+
+    @jwt.revoked_token_loader
+    # build in decorator for a function that will specify the revoked token message (flask message)
+    def revoked_token_callback(jwt_header, jwt_payload):
+        return (jsonify({"description": "The token has been revoked", "error": "token_revoked"}), 401)
+
+    @jwt.additional_claims_loader
+    # "identity" in function args is coming from identity param that was used  in jwt token creation
+    def add_claims_to_jwt(identity):
+        if identity == 1:
+            return {"is_admin": True}
+        return {"is_admin": False}
+
+    @jwt.expired_token_loader
+    def expired_token_callback(jwt_header, jwt_payload):
+        return {
+            jsonify({"message": "The token has expires.", "error": "token_expired"}),
+            401,
+        }
+
+    @jwt.invalid_token_loader
+    def invalid_token_callback(error):
+        return {
+            jsonify({"message": "The signature verification failed", "error": "token_invalid"}),
+            401,
+        }
+
+    @jwt.unauthorized_loader
+    def missing_token_callback(error):
+        return {
+            jsonify({"description": "Request does not contain an access token", "error": "authorization_required"}),
+            401,
+        }
+
     # initiate api
-    api = Api(app)    
+    api = Api(app)
 
     api.register_blueprint(StoreBlueprint)
     api.register_blueprint(ItemBlueprint)
     api.register_blueprint(TagBlueprint)
     api.register_blueprint(UserBlueprint)
-    
+
     return app
--- resources/user.py
+++ resources/user.py
@@ -1,75 +1,95 @@
 from flask.views import MethodView
+from flask_jwt_extended import create_access_token, get_jwt, jwt_required
 from flask_smorest import Blueprint, abort
-from flask_jwt_extended import create_access_token, jwt_required
 from passlib.hash import pbkdf2_sha256
-from sqlalchemy.exc import SQLAlchemyError, IntegrityError
+from sqlalchemy.exc import IntegrityError, SQLAlchemyError
 
 from db import db
-from models import UserModel
+from models import BlocklistModel, UserModel
 from schemas import UserSchema
 
 blp = Blueprint("users", __name__, description="Operation on users")
 
+
 @blp.route("/register")
 class UserRegister(MethodView):
     @blp.arguments(UserSchema)
-    @blp.response(201,UserSchema)
-    def post(self,validated_user_data):
+    @blp.response(201, UserSchema)
+    def post(self, validated_user_data):
         user = UserModel(
-            username = validated_user_data["username"],
-            password = pbkdf2_sha256.hash(validated_user_data["password"])
+            username=validated_user_data["username"], password=pbkdf2_sha256.hash(validated_user_data["password"])
         )
-        
+
         try:
             db.session.add(user)
             db.session.commit()
-            
-        #uniqueness validation exception
+
+        # uniqueness validation exception
         except IntegrityError:
-            abort(400,message="User already exists")
-        #any other errors    
+            abort(400, message="User already exists")
+        # any other errors
         except SQLAlchemyError:
             abort(500, message="An error occurred while inserting the item")
 
-        return {"message": "User added successfully"},201
+        return {"message": "User added successfully"}, 201
 
 
 @blp.route("/login")
 class UserLogin(MethodView):
     @blp.arguments(UserSchema)
-    def post(self,validated_user_data):
-        user = UserModel.query.filter(
-            UserModel.username == validated_user_data['username']
-            ).first()
-        
-        if user and  pbkdf2_sha256.verify(validated_user_data['password'],user.password):
-            #creates an access token that contains USER ID for further identification
+    def post(self, validated_user_data):
+        user = UserModel.query.filter(UserModel.username == validated_user_data["username"]).first()
+
+        if user and pbkdf2_sha256.verify(validated_user_data["password"], user.password):
+            # creates an access token that contains USER ID for further identification
             access_token = create_access_token(identity=user.id)
             return {"access_token": access_token}
-        
+
         return {"message": "bad password or nonexisting user"}, 500
+
+
+@blp.route("/logout")
+class UserLogout(MethodView):
+    @jwt_required()
+    def post(self):
+        # "get_gwt()['jti'] -> this extracts the token ID from the whole token"
+        token = BlocklistModel(jti=get_jwt()["jti"])
+        
+        
+        try:
+            db.session.add(token)
+            db.session.commit()
+
+        # uniqueness validation exception
+        except IntegrityError:
+            abort(400, message="Token already exists")
+        # any other errors
+        except SQLAlchemyError:
+            abort(500, message="An error occurred while revoking the token")
+
+        return {"message": "token revoked and logged out"}, 201
 
 
 @blp.route("/users")
 class UserList(MethodView):
     @blp.response(200, UserSchema(many=True))
     def get(self):
-        users =UserModel.query.all()
+        users = UserModel.query.all()
         return users
 
-    
+
 @blp.route("/user/<int:user_id>")
 class User(MethodView):
     @blp.response(200, UserSchema)
     def get(self, user_id):
         user = UserModel.query.get_or_404(user_id)
         return user
-    
-    @jwt_required(refresh=True)
+
+    @jwt_required()
     def delete(self, user_id):
         user = UserModel.query.get_or_404(user_id)
-      
+
         db.session.delete(user)
         db.session.commit()
-         
-        return {"message": "User deleted successfully"},200
+
+        return {"message": "User deleted successfully"}, 200
